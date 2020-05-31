e15ffee5923e417526aa3e76c6c26b3aa7383895
Yavor Papazov <yavorchap@gmail.com>
2020-05-31 18:43:43 +0200

    Created py file

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+colors = ['blue', 'green', 'red']
+author = {'name': 'Hank', 'color': 'green'}
+print(f"The author's name is {author['name']}.\nHis favorite color is {author['color']}.\n")
+print("The current colors are:")
+for color in colors:
+	print(color)
