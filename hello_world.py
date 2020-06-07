d9be5ec6b190bc65865a601a6db2b1716f20f397
yavorpapazov <66213764+yavorpapazov@users.noreply.github.com>
2020-06-07 15:20:17 +0200

    Delete hello_world.py

--- hello_world.py
+++
@@ -1,7 +0,0 @@
-colors = ['blue', 'green', 'red']
-author = {'name': 'Hank', 'color': 'green'}
-print(f"The author's name is {author['name']}.\nHis favorite color is {author['color']}.\n")
-print("The current colors are:")
-for color in colors:
-	print(color)
-print("Hello from github!")
