c45bbff00b7d978d982917c5036fc5138dcb7477
yavorpapazov <66213764+yavorpapazov@users.noreply.github.com>
2022-07-10 16:50:55 -0500

    Add files via upload

---
+++ MyBuildings/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import './index.css'
+import './style.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
